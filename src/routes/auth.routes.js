8f0b37e788c45b584727fa3e74e791580a0a6143
emanuelhardwell <emanuelnoe16@gmail.com>
2022-06-03 20:50:28 -0500

    [Auth] se agregaron las validaciones a las rutas de autenticación

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const { Router } = require("express");
 const { validateJWT } = require("../middlewares/validate-jwt");
+const { check } = require("express-validator");
+const { validateInputs } = require("../middlewares/validate-inputs");
 
 const {
   createUser,
@@ -14,14 +16,33 @@ const router = Router();
  * @url /new
  * create user
  */
-router.post("/new", createUser);
+router.post(
+  "/new",
+  [
+    check("name", "the name is required").notEmpty(),
+    check("lastname", "the lastname is required").notEmpty(),
+    check("lastname2", "the lastname2 is required").notEmpty(),
+    check("email", "the email is required").notEmpty(),
+    check("password", "the password is required").isLength({ min: 6 }),
+    validateInputs,
+  ],
+  createUser
+);
 
 /**
  * API /api/v1/auth
  * @url /login
  * login user
  */
-router.post("/login", loginUser);
+router.post(
+  "/login",
+  [
+    check("email", "the email is required").notEmpty(),
+    check("password", "the password is required").isLength({ min: 6 }),
+    validateInputs,
+  ],
+  loginUser
+);
 
 /**
  * API /api/v1/auth
