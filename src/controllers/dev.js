556ed9c553f99a18fb350b5fc5382c9e196e54e8
emanuelhardwell <emanuelnoe16@gmail.com>
2023-02-02 23:52:45 -0600

    commit 2 dev

--- src/controllers/dev.js
+++ src/controllers/dev.js
@@ -3,4 +3,8 @@
 const dev = "Hola voy hacer un commit de DEV";
 const dev2 = "voy hacer el primer commit";
 
+const dev3 = "voy hacer el tercer(3) commit";
+const dev3Plus = "voy hacer el tercer(3) commitPlus";
+const dev3PlusPro = "voy hacer el tercer(3) commitPlus Pro";
+
 console.log(dev);
