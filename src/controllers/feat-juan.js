769e72aeafac825f8e41154870b35661485fb076
emanuelhardwell <emanuelnoe16@gmail.com>
2023-02-02 23:22:25 -0600

    se agrego el commit feat-juan

---
+++ src/controllers/feat-juan.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let juan = "juan";
+let mensaje = "quire automatizar sus test";
+let mensaje2 = "para tener mejor optimizado su funcionamiento";
+
+respuesta = juan + " " + mensaje + " " + mensaje2;
+console.log(respuesta);
